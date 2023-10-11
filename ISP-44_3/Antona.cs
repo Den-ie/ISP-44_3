3f24d1d2a118b915f217211efc2d6683a52eb0f3
qwerty <hg@guy>
2023-10-11 14:00:07 +0300

    Создан класс Антоха

---
+++ ISP-44_3/Antona.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP_44_3
+{
+    internal class Antona
+    {
+        // Метод внутреннего класса, вычисляющий выражение 3+5-3+8
+        public int CalculateExpression()
+        {
+            int result = 3 + 5 - 3 + 8;
+            return result;
+        }
+    }  
+}

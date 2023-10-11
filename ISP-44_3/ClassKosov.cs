116e7122e2f0f2062f0fb60e979f2a8c64204060
esfdsef <awdawd@gmail>
2023-10-11 13:57:23 +0300

    Method done

---
+++ ISP-44_3/ClassKosov.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP_44_3
+{
+    internal class ClassKosov
+    {
+        public int Method()
+        {
+            return -100;
+        }
+    }
+}
